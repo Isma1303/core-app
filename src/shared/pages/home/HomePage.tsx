1a9b392def19fb046b20f68b7ac65ee7612af6d9
Isma1303 <liquezismael@gmail.com>
2025-09-30 11:50:09 -0600

    :alien_monster: se agrego el logo de solcomp en home page

--- src/shared/pages/home/HomePage.tsx
+++ src/shared/pages/home/HomePage.tsx
@@ -1,5 +1,9 @@
 import './homePage.scss'
 
 export const HomePage = (): JSX.Element => {
-    return <div className="d-flex justify-content-center align-items-center" style={{ height: '80vh' }}></div>
+    return (
+        <div className="d-flex justify-content-center align-items-center" style={{ height: '80vh' }}>
+            <img src="/assets/logo.png" alt="" style={{ width: '100%' }} />
+        </div>
+    )
 }
